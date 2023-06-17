d9775d03bf32cd37e38e1709ad9ad5d4ad8f85e7
mr-propre-debug <64852608+mr-propre-debug@users.noreply.github.com>
2023-06-18 01:46:25 +0200

    idk what i added but multple things

--- Main.py
+++ Main.py
@@ -15,6 +15,8 @@ import random
 import threading
 from threading import Timer
 
+from math import log, floor
+
 #initialisation de pygame et pour l'affichage
 pygame.init()
 clock = pygame.time.Clock()
@@ -23,6 +25,9 @@ clock = pygame.time.Clock()
 width = 1280
 height = 720
 screen = pygame.display.set_mode((width, height))
+
+# backgrounds
+bgMenu = bgMenu = pygame.image.load("assets/images/bgMenu.jpg")
 
 #volume du jeu
 volume = 1.0
@@ -56,11 +61,32 @@ def game_menu():
     moneyMultiplier = data["moneyMultiplier"]
     
     def addMoney(numChick, moneyMul):
+        data["money"] = money
+        data["chickens"] = chickens
+        data["eggs"] = eggs
+        data["moneyMultiplier"] = moneyMultiplier
         Money = numChick * 2 * moneyMul
         return Money
     
-    timeToAddMoney = USEREVENT
+    def human_format(number):
+        
+        units = ["", "K", "M", "B", "t", "q", "Q", "s", "S", "o", "n", "d", "U", "D", "T", "Qt", "Qd", "Sd", "St", "O", "N", "v", "c"]
+        k = 1000.0
+        if number == 0:
+            return '%.2f%s' % (number, units[0])
+        magnitude = int(floor(log(number, k)))
+        return '%.2f%s' % (number / k**magnitude, units[magnitude])
+
+    def save(data):
+        with open('data/data.json', 'w') as f:
+            json.dump(data, f)
+        f.close()
+
+    timeToAddMoney = USEREVENT + 1
     pygame.time.set_timer(timeToAddMoney, 1000)
+
+    autoSave = USEREVENT
+    pygame.time.set_timer(autoSave, 30000)
 
     while True:
         # screen.blit(bgGame, (0,0))
@@ -69,8 +95,8 @@ def game_menu():
         game_mouse_pos = pygame.mouse.get_pos()
 
         generateChicken = Button(image=pygame.image.load("assets/images/Play Rect.png"), pos=(640,670), text_input="More chicken", font=get_font(65), base_color=(215, 252, 212), hovering_color=(255, 255, 255))
-        moneyDisplay = Button(image=pygame.image.load("assets/images/Play Rect.png"), pos=(640,50), text_input= f"{money} $", font=get_font(65), base_color=(215, 252, 212), hovering_color=(255, 255, 255))
-        chickensDisplay = Button(image=pygame.image.load("assets/images/Play Rect.png"), pos=(640,159), text_input=f"chickens {chickens}", font=get_font(65), base_color=(215, 252, 212), hovering_color=(255, 255, 255))
+        moneyDisplay = Button(image=pygame.image.load("assets/images/Play Rect.png"), pos=(640,50), text_input=f"{human_format(money)} $", font=get_font(65), base_color=(215, 252, 212), hovering_color=(255, 255, 255))
+        chickensDisplay = Button(image=pygame.image.load("assets/images/Play Rect.png"), pos=(640,159), text_input=f"chickens : {human_format(chickens)}", font=get_font(45), base_color=(215, 252, 212), hovering_color=(255, 255, 255))
 
         for button in [generateChicken, moneyDisplay, chickensDisplay]:
             button.changeColor(game_mouse_pos)
@@ -86,26 +112,34 @@ def game_menu():
                 #pour quitter
                 if generateChicken.checkForInput(game_mouse_pos):
                     chickens += 1
+                    eggs += chickens * 2
                 
             #pour quitter avec echap
             elif event.type == KEYDOWN:
                 if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
+                
+                if event.key == K_DOLLAR:
+                    money += 100_000_000
 
             elif event.type == timeToAddMoney:
                 money += addMoney(chickens, moneyMultiplier)
+            
+            elif event.type == autoSave:
+                save(data)
 
             #pour actualiser l'affichage
             pygame.display.flip()
             clock.tick(60) #nombre d'image par seconde
 
 def main_menu():
+    
     pygame.display.set_caption("Menu")
     screen.fill((0,0,0))
 
     while True:
-        # screen.blit(bgMenu, (0,0))
+        screen.blit(bgMenu, (0,0))
 
         #pour avoir la position de la souris
         menu_mouse_pos = pygame.mouse.get_pos()
