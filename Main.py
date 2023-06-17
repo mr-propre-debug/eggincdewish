5a84c35f766a4febf82a9b80b1be7ac77a5566cf
mr-propre-debug <64852608+mr-propre-debug@users.noreply.github.com>
2023-06-17 21:38:41 +0200

    added button module

--- Main.py
+++ Main.py
@@ -1,3 +1,25 @@
-import tkinter
-import pygame
+import pygame, sys
+from pygame.locals import *
+
+from button import Button
+
+import tkinter as tk
+from tkinter import messagebox
+
+import os
+
 import json
+
+import random
+
+#initialisation de pygame et pour l'affichage
+pygame.init()
+clock = pygame.time.Clock()
+
+#setup de l'affichage
+width = 1280
+height = 720
+screen = pygame.display.set_mode((width, height))
+
+def menu():
+    pygame.display.set_caption("Menu")
