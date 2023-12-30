3fa8062d64f55bd4e02c845c56990dcedce8780b
mr-propre-debug <paul.ferrand0502@gmail.com>
2023-12-30 22:50:10 +0100

    surchage str dans wallet

--- wallet.py
+++ wallet.py
@@ -1,28 +1,14 @@
 class Wallet:
 
-    def __init__(self, balance, withdrawAmount, depositAmount):
+    def __init__(self, balance=0):
 
         self.balance = balance
-        self.withdrawAmount = withdrawAmount
-        self.depositAmount = depositAmount
 
     def _get_balance(self):
         return self._balance
     
     def _set_balance(self, balance):
         self._balance = balance
-
-    def _get_withdrawAmount(self):
-        return self._withdrawAmount
-    
-    def _set_withdrawAmount(self, withdrawAmount):
-        self._withdrawAmount = withdrawAmount
-    
-    def _get_depositAmount(self):
-        return self._depositAmount
-    
-    def _set_depositAmount(self, depositAmount):
-        self._depositAmount = depositAmount
 
     def deposit(self, amount : float):
         self._ += amount
@@ -33,6 +19,7 @@ class Wallet:
         else:
             self._balance -= amount
 
+    def __str__(self):
+        s = str(self.balance) + 
+
     balance = property(_get_balance, _set_balance)
-    withdrawAmount = property(_get_withdrawAmount, _set_withdrawAmount)
-    depositAmount = property(_get_depositAmount, _set_depositAmount)
